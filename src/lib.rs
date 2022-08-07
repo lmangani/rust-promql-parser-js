4d072b3de3c53d193ae53db391422b6030ffee09
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2022-08-07 18:48:47 +0200

    resync

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,12 @@ pub fn promql_parse(query: String) -> String {
     let (_, ast) = promql::parse_expr(&query).unwrap();
     let mut x: String = format!("{:?}", ast);
     // there has to be a better way to remove the Expression types?
-    x = x.replace("FunCallExpr(FunCall ","").replace("SubQueryExpr(SubQueryExpr","").replace("SubQueryExpr(SubQueryExpr","").replace("VectorExprVector", "").replace("})","}").replace("VectorExpr(Vector","").replace("LabelMatcher","").replace("Some(AggregationModifier","").replace("None","false");
+    x = x.replace("FunCallExpr(FunCall ","");
+    x = x.replace("SubQueryExpr(SubqueryExpr","");
+    x = x.replace("VectorExprVector", "");
+    x = x.replace("LabelMatcher","").replace("Some(AggregationModifier","").replace("None","false");
+    x = x.replace("})","}").replace("VectorExpr(Vector","");
+    x = x.replace("Some(","\"").replace("s)", "s\"");
     return x;
 }
 
@@ -14,8 +19,19 @@ pub fn promql_parse(query: String) -> String {
 #[test]
 fn check_parser() {
     let (_, ast) = promql::parse_expr("min_over_time(rate(http_requests_total[5m])[30m:1m])").unwrap();
+    print_type_of(&ast);
     let mut x: String = format!("{:?}", ast);
-    x = x.replace("FunCallExpr(FunCall ","").replace("SubQueryExpr(SubQueryExpr","").replace("SubQueryExpr(SubQueryExpr","").replace("VectorExprVector", "").replace("})","}").replace("VectorExpr(Vector","").replace("LabelMatcher","").replace("Some(AggregationModifier","").replace("None","false");
+    x = x.replace("FunCallExpr(FunCall ","");
+    x = x.replace("SubQueryExpr(SubqueryExpr","");
+    x = x.replace("VectorExprVector", "");
+    x = x.replace("LabelMatcher","").replace("Some(AggregationModifier","").replace("None","false");
+    x = x.replace("})","}").replace("VectorExpr(Vector","");
+    x = x.replace("Some(","\"").replace("s)", "s\"");
+    // print_type_of(&x);
     println!("{}", x);
     assert_eq!(x, x); // dummy test
+}
+
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>())
 }
