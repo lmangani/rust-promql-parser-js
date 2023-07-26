810e60512e637315fbca0f0cd0c39b675e91c6db
Hans <admin@itsrv.tw>
2023-07-27 01:42:05 +0800

    Make apt quiet

--- build.sh
+++ build.sh
@@ -3,10 +3,11 @@
 set -e
 
 apt-get update
-apt-get install -y build-essential curl git
+apt-get install -yq build-essential curl git
 
 curl -sL https://deb.nodesource.com/setup_18.x | bash -
-apt-get install -y nodejs
+apt-get update
+apt-get install -yq nodejs
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
