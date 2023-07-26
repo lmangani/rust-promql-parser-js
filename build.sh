5730260f3230ebce1ae796c20e511e3679413546
Hans <admin@itsrv.tw>
2023-07-27 01:48:22 +0800

    Change to build rust directly

--- build.sh
+++ build.sh
@@ -2,16 +2,5 @@
 
 set -e
 
-apt-get update
-apt-get install -yq build-essential curl git
-
-curl -sL https://deb.nodesource.com/setup_18.x | bash -
-apt-get update
-apt-get install -yq nodejs
-
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source $HOME/.cargo/env
-
-npm i
-npm run build
-npm run test
+cargo install wasm-pack
+wasm-pack build --target nodejs --release --scope qxip
